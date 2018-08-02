5786b14161239aa8061abc47cd65b39430903c51
DinhThanhNghia <nghia38511@donga.edu.vn>
2018-08-02 09:02:09 +0700

    Replace Caculator

---
+++ Operation/Operation/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Operation.Models;
+
+namespace Operation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(OparationViewModel model, string command)
+        {
+            if(command == "add")
+            {
+                model.Result = model.A + model.B;
+            }
+            if(command == "sub")
+            {
+                model.Result = model.A - model.B;
+            }
+            if(command == "mul")
+            {
+                model.Result = model.A * model.B;
+            }
+            if (command == "div")
+            {
+                model.Result = model.A / model.B;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Operation/Operation/Models/OparationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Operation.Models
+{
+    public class OparationViewModel
+    {
+        public double A
+        {
+            get;
+            set;
+        }
+        public double B
+        {
+            get;
+            set;
+        }
+        public double Result
+        {
+            get;
+            set;
+        }
+    }
+}
